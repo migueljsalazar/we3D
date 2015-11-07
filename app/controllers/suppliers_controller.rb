643b3a7e615d9e15dbe81243d9f959845440475c
13um45 <christian.leumas@icloud.com>
2015-11-07 18:12:49 -0500

    added create to supplier controller

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -2,4 +2,22 @@ class SuppliersController < ApplicationController
   def new
     @supplier = Supplier.new
   end
+
+  def create
+    @supplier = Supplier.new supplier_params
+
+    if @supplier.save
+      redirect_to root_path, notice: "Created Supplier"
+    else
+      render action: "new"
+    end
+  end
+
+  private
+    def supplier_params
+      params.
+        require(:supplier).
+        permit(:username, :password, :password_confirmation)
+  end
+
 end
