0dc8519b9249a0d59d41562648ea5eb32fdf51a6
13um45 <christian.leumas@icloud.com>
2015-11-07 20:21:29 -0500

    added supplier to sessions allowing both suppliers and designers to signup on the same sessions page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
   def create
     @designer = Designer.
       find_by(username: params[:username]).
+      try(:authenticate, params[:password]) ||
+
+      @supplier = Supplier.
+      find_by(username: params[:username]).
       try(:authenticate, params[:password])
 
     if @designer
       session[:designer_id] = @designer.id
       redirect_to products_path
+
+    elsif
+      @supplier
+      session[:supplier_id] = @supplier.id
+      redirect_to products_path
+
     else
       flash[:notice] = "Form is invalid"
       flash[:color]= "invalid"
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,6 @@
 class Campaign < ActiveRecord::Base
 belongs_to :product
 belongs_to :designer
+belongs_to :supplier
 end
 
