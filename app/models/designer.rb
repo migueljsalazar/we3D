6c3c295aac6ecc574039493d7dd55a0e8c52add7
13um45 <christian.leumas@icloud.com>
2015-11-09 15:43:27 -0500

    added validate username_in_use

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -7,6 +7,7 @@ class Designer < ActiveRecord::Base
   validates :email, :presence => true, :format => /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/
   validates :password, :confirmation => true #password_confirmation attr
   validates_length_of :password, :in => 6..20, :on => :create
+  validate :username_in_use
 
   before_save :to_lower
   before_create :to_lower
