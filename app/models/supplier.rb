0e413809a5816f101a7dff25fbcf116bb14d578e
13um45 <christian.leumas@icloud.com>
2015-11-09 14:39:19 -0500

    supplier usernames are now downcase in the supplier model. added validationn for username in both supplier and designer model.

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,4 +1,23 @@
 class Supplier < ActiveRecord::Base
   has_secure_password
   has_many :campaigns
+  validate :username_in_use
+
+  before_save :to_lower
+before_create :to_lower
+
+def to_lower
+  self.username = self.username.downcase
+end
+
+
+
+  private
+
+  def username_in_use
+    if Supplier.where("lower(username) = ?", self.username.downcase).first
+      errors.add(:username, "in use")
+    end
+  end
+
 end
