0d058b2773a5b709d1e51586148a1a33e1922e4c
13um45 <christian.leumas@icloud.com>
2015-11-10 14:07:40 -0500

    fixed validation bug.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ActiveRecord::Base
 has_many :campaigns
 belongs_to :designer
 has_attached_file :product_image
-validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+validates_attachment_content_type :product_image, :content_type => /\Aimage\/.*\Z/
 end
