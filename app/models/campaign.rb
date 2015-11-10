f862f28bf49360374bd052812ea233d87fb8cd87
Miguel Salazar <migueljsalazar@icloud.com>
2015-11-10 17:53:23 -0500

    removed extraneous copes from the campaign controller

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,10 +4,6 @@ belongs_to :designer
 belongs_to :supplier
 has_many :orders
 
-scope :active, -­> { where(active: true) }
-scope :cohort, ­->(cohort_num) { where(cohort: cohort_num) }
-scope :active_cohort, ­->(cohort_num) { active.cohort cohort_num }
-
 scope :start, -> {where(start: true)}
 scope :available, -> {where(available: true)}
 scope :on, -> {where(status: "on")}
