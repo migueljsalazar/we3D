4f8100a5eb2fb6eef7c642e4fa71313dc14e2e42
Miguel Salazar <migueljsalazar@icloud.com>
2015-11-10 17:52:51 -0500

    Changed the availability field for campaigns to available as a better field name. Added scopes to the campaign controller for start, status, and available.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -4,5 +4,15 @@ belongs_to :designer
 belongs_to :supplier
 has_many :orders
 
+scope :active, -­> { where(active: true) }
+scope :cohort, ­->(cohort_num) { where(cohort: cohort_num) }
+scope :active_cohort, ­->(cohort_num) { active.cohort cohort_num }
+
+scope :start, -> {where(start: true)}
+scope :available, -> {where(available: true)}
+scope :on, -> {where(status: "on")}
+scope :backed, -> {where(status: "backed")}
+scope :off, -> {where(status: "off")}
+
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109221938) do
+ActiveRecord::Schema.define(version: 20151110224728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20151109221938) do
   create_table "campaigns", force: :cascade do |t|
     t.string   "status"
     t.boolean  "start"
-    t.boolean  "availability"
     t.integer  "goal"
     t.integer  "profit"
     t.string   "title"
@@ -27,8 +26,9 @@ ActiveRecord::Schema.define(version: 20151109221938) do
     t.datetime "length"
     t.binary   "image"
     t.integer  "price"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.boolean  "available"
   end
 
   create_table "customers", force: :cascade do |t|
