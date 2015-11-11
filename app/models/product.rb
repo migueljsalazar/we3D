8f0c1eb5a3c905c57cdfdd6f8ca4c6b12085b8a0
13um45 <christian.leumas@icloud.com>
2015-11-11 12:40:25 -0500

    added the ability to upload 3d files and view on product show page.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ActiveRecord::Base
   has_many :campaigns
   belongs_to :designer
   has_attached_file :product_image
-  validates_attachment_content_type :product_image, :content_type => /\Aimage\/.*\Z/
+  do_not_validate_attachment_file_type :product_image, :content_type=>['file/stl']
 end
