100dfcacbbab2dccaa12d099c2f2a0df64aefc37
13um45 <christian.leumas@icloud.com>
2015-11-11 18:00:32 -0500

    removed some values from product params in product controller and replaced them with product_image which fixed bug in product show page. updated rendering settings so 3d objects are now rendered at a higher quality. changed color of background and 3d models in rendering object on show page. removed renderer from product new page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -57,6 +57,6 @@ class ProductsController < ApplicationController
 
 
   def product_params
-    params.require(:product).permit(:name, :x, :y, :z, :color, :base_cost, :profit, :product_image_file_name, :product_image_content_type, :product_image_file_size, :product_image_updated_at, :designer_id)
+    params.require(:product).permit(:name, :x, :y, :z, :color, :base_cost, :profit, :product_image, :designer_id)
   end
 end
