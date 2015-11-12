2409d15d7e4feb86a7d7155196892218e604a675
Miguel Salazar <migueljsalazar@icloud.com>
2015-11-12 18:11:36 -0500

    Found and deleted more customer bugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def new
     if current_designer || current_supplier || current_customer
       redirect_to root_path
+    end
   end
 
   def create
@@ -13,21 +14,12 @@ class SessionsController < ApplicationController
       find_by(username: params[:username].downcase).
       try(:authenticate, params[:password])
 
-    @customer = Customer.
-      find_by(username: params[:username].downcase).
-      try(:authenticate, params[:password])
-
-
     if @designer
       session[:designer_id] = @designer.id
       redirect_to new_product_path
 
     elsif @supplier
       session[:supplier_id] = @supplier.id
-      redirect_to :back
-
-    elsif  @customer
-      session[:customer_id] = @customer.id
       redirect_to :back
 
     else
@@ -37,11 +29,11 @@ class SessionsController < ApplicationController
     end
   end
 
-    def destroy
-      session[:designer_id] = nil
-      session[:supplier_id] = nil
-      session[:customer_id] = nil
-      redirect_to :back
-    end
+  def destroy
+    session[:designer_id] = nil
+    session[:supplier_id] = nil
+    session[:customer_id] = nil
+    redirect_to :back
+  end
 
 end
