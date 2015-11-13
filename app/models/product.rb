6febe0e7c1526bcdc56f4f9c3317fa704a1002e0
13um45 <christian.leumas@icloud.com>
2015-11-13 02:26:18 -0500

    fix bug for deleting product. when product is deleted campaign gets deleted as well.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-  has_many :campaigns
-  belongs_to :designer, dependent: :destroy
+  has_many :campaigns, dependent: :destroy
+  belongs_to :designer
   has_attached_file :object
   validates_attachment_content_type :object, :content_type=>['application/octet-stream']
 
