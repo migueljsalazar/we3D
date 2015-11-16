48178ce0ae0074be9cd443e4735e49f16ac7b130
Miguel Salazar <migueljsalazar@icloud.com>
2015-11-16 15:09:50 -0500

    Added the modern business template, and applied the change necessary to welcome index.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,19 +8,32 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
     @campaign = @order.campaign
+    @amount = @campaign.price*100
   end
 
   def create
+
     @order = Order.new(order_params).merge(email: stripe_params["stripeEmail"],
                                                                card_token: stripe_params["stripeToken"])
     raise "Please, check order errors" unless @order.valid?
     @order.process_payment
     @order.save
     redirect_to @order, notice: 'order was successfully created.'
-    raise
-  rescue => e
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+    rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to :back
+
   end
 
   def edit
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,8 @@ Rails.configuration.stripe = {
   :secret_key      => ENV['SECRET_KEY']
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+Stripe.api_key = Rails.env.production? ?
+                 Rails.configuration.stripe[:secret_key] :
+                 Settings[Rails.env]['stripe']['secret_key']
