0e60c2d861e35f4b516119a574f19ffeef1a54a8
13um45 <christian.leumas@icloud.com>
2015-11-18 17:26:09 -0500

    implemented some bug fixes.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @campaigns = Campaign.unavailable.on
+
+    @popular_campaign = @campaigns.first # TODO: implement popular
   end
 end
